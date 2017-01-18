d13dc4a27491bd936404f8ac09b97653a9d5ac0d
Fialek Fialek <Fialek Fialek>
2017-01-18 19:58:47 +0100

    init commit

---
+++ projekt/Projekt/Main.cpp
@@ -0,0 +1,17 @@
+#include <conio.h>
+#include<Windows.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char litera;
+	int rozmiar;
+	int pozycja[2];
+	int *figura;
+
+
+	return 0;
+}
+
